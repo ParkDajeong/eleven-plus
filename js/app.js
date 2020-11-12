5121f58309ede2718e438bfdd87ceefefc1b70aa
ParkDajeong <sobeast980@gmail.com>
2020-11-12 18:51:59 +0900

    message

---
+++ js/app.js
@@ -0,0 +1,74 @@
+// Mobile Dropdown Menu
+const btnMenu = document.querySelector(".mobile-menu__item--menu");
+const btnClose = document.querySelector(".mobile-dropdown-menu__button--close");
+const mobileDropdownMenu = document.querySelector(".mobile-dropdown-menu");
+
+function openDropdownMenu() {
+  mobileDropdownMenu.style.maxHeight = "500px";
+}
+
+function closeDropdownMenu() {
+  mobileDropdownMenu.style.maxHeight = "0";
+}
+
+btnMenu.addEventListener("click", openDropdownMenu);
+btnClose.addEventListener("click", closeDropdownMenu);
+
+// Slider
+const slider = document.querySelector(".slider");
+const sliderItems = document.querySelectorAll(".slider__item");
+const sliderLen = sliderItems.length;
+let currentIdx = 1;
+const sliderSpeed = 300;
+let sliderWidth = slider.offsetWidth;
+// Slider Pagination
+const sliderPagination = document.querySelector(".slider-pagination");
+const sliderBullets = document.querySelectorAll(".slider-pagination__bullet button");
+
+function moveSliderImages(speed) {
+  slider.style.transition = speed + "ms";
+  slider.style.transform = "translateX(" + (-(sliderWidth * currentIdx++)) + "px)";
+}
+
+function moveSliderPagination() {
+  for(let i=0; i<sliderBullets.length; i++) {
+    sliderBullets[i].classList.remove("active");
+  }
+  if(currentIdx >= sliderLen - 1) {
+    sliderBullets[0].classList.add("active");
+  } else {
+    sliderBullets[currentIdx].classList.add("active");
+  }
+}
+
+function roopSlider() {
+  setInterval(function() {
+    moveSliderPagination();
+    moveSliderImages(sliderSpeed);
+    
+    if(currentIdx === sliderLen) {
+      setTimeout(function() {
+        currentIdx = 0;
+        moveSliderPagination();
+        moveSliderImages(0);
+      }, 200)
+    }
+  }, 3000);
+}
+
+function clickSliderPagination(e) {
+  const clickedEl = e.target;
+  if(clickedEl.tagName !== "BUTTON") return;
+
+  currentIdx = clickedEl.dataset.index;
+  moveSliderPagination();
+  moveSliderImages(0);
+}
+
+window.addEventListener("resize", function() {
+  sliderWidth = slider.offsetWidth;
+});
+window.addEventListener("load", roopSlider);
+
+sliderPagination.addEventListener("click", clickSliderPagination);
+
